34138c9cf919aa72102048ecd135d05de040fa05
Pierreg <pierre.imbaud@gmail.com>
2021-02-18 10:17:06 +0100

    algo delivered

---
+++ lib/square.py
@@ -0,0 +1,199 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""contains inFile class, instances resulting of file
+   read at the start of the script
+   The script ability to read many files induces data
+   (plateau, but equally "vide", "obstacle", "plein") instanciated many times:
+   might as well live in a class instance
+"""
+
+import collections
+
+
+# coordonnées d'un point sur le plateau. les x sont les abcisses,
+# ils numérotent les colonnes,
+# les y numérotent les lignes, 0 est la premiere ligne, 1 la seconde...
+Point = collections.namedtuple('Point', ['x', 'y'])
+
+
+class inFile():
+    """ Instances will contain data read from file, and data processed
+    methods will implement the algorithm
+    """
+
+    def __init__(self, input):
+        """ create instance from input file
+        """
+        self.fname = input
+        with open(input, 'r') as inf:
+            spl = spec_line = inf.readline().strip()
+            # fix! might strip leading spaces, if eg vide char is space
+            if len(spl) < 4:
+                raise ValueError('premiere ligne %s pas assez longue' % spl)
+            nb_lines, self.vide, self.obstacle, self.plein = \
+                spl[:-3], spl[-3], spl[-2], spl[-1]
+            try:
+                self.nb_lines = int(nb_lines)
+            except ValueError:
+                raise ValueError('%s: int attendu' % nb_lines)
+            # lire le plateau: tuple de tuples
+            self.plateau = tuple([tuple(x.strip())
+                                  for x in inf][:self.nb_lines])
+            self.recouche()
+            self.nb_cols = self.check()
+
+    def recouche(self):
+        """ recalcule l'attribut plateau_couche
+            mis dans une methode pour aider les TU, qui patchent plateau
+        """
+        self.plateau_couche = tuple(zip(*self.plateau))
+
+    def check(self):
+        """teste le plateau apres lecture.
+        retourne le nombre de colonnes
+        """
+
+        if len(self.plateau) != self.nb_lines:
+            raise ValueError(
+                f"{self.fname}: {self.nb_lines} attendues, " +
+                f"{len(self.plateau)} lignes")
+
+        authorized_chars = set((self.vide, self.obstacle))
+
+        nb_cols = len(self.plateau[0])
+        for line in self.plateau:
+            if len(line) != nb_cols:
+                raise ValueError(f"{self.fname}: lignes inegales")
+            chars = set(line)
+            if not chars <= authorized_chars:
+                raise ValueError(f"{self.fname}: caracteres non attendus")
+        return nb_cols
+
+    def value(self, point):
+        """valeur du datum en ce point
+        """
+        return self.plateau[point.y][point.x]
+
+    def barre_h(self, point, lg):
+        """ retourne la barre horizontale partant de point,
+        dirigee à droite, de longueur lg
+        """
+        return self.plateau[point.y][point.x:point.x + lg]
+
+    def barre_v(self, point, lg):
+        """ retourne la barre verticale partant de point,
+        dirigee vers le bas, de longueur lg
+        """
+        return self.plateau_couche[point.x][point.y:point.y + lg]
+
+    def barre_sh(self, point, lg):
+        """ comme barre_h, mais string (pour debug)
+        """
+        return ''.join(self.barre_h(point, lg))
+
+    def barre_sv(self, point, lg):
+        """ comme barre_v, mais string (pour debug)
+        """
+        return ''.join(self.barre_v(point, lg))
+
+    def scan_carre(self, coin):
+        """scan du plus grand carre depuis un coin
+
+        version incrementale
+        return la dimension du carré. 0 indique que le coin est un obstacle
+        """
+
+        """max_value: taille max possible sans obstacle:
+        on atteint les limites du plateau
+        """
+        max_value = min(self.nb_lines - coin.y, self.nb_cols - coin.x)
+
+        obstacle = self.obstacle
+        if self.value(coin) == obstacle:
+            return 0
+
+        for taille in range(0, max_value):
+            diagonale = self.plateau[coin.y + taille][coin.x + taille]
+            if diagonale == obstacle:
+                return taille
+            coin_bas = Point(coin.x, coin.y + taille)
+            if obstacle in self.barre_h(coin_bas, taille):
+                return taille
+
+            coin_droit = Point(coin.x + taille, coin.y)
+            if obstacle in self.barre_v(coin_droit, taille):
+                return taille
+        try:
+            return taille + 1
+        except NameError:
+            return 0            # on était au bord, pas passé dans le for.
+
+    def scan_brutal(self):
+        """ invoque scan_carre sur tous les points du plateau
+
+        stocke les plus grands trouves (sans garder les petits)
+        puis choisit et stocke le plus grand dans result
+        """
+
+        found = []              # plus grands carres trouves
+        max_size = 0
+
+        for x in range(self.nb_cols):
+            for y in range(self.nb_lines):
+                coin = Point(x, y)
+                taille = self.scan_carre(coin)
+                if taille > max_size:
+                    found = [(coin, taille)]
+                    max_size = taille
+                elif taille == max_size:
+                    found.append((coin, taille))
+
+        if not found:
+            self.result = None  # possible s'il n'y a que des obstacles
+            return
+        assert found
+        if len(found) == 1:
+            self.result = found[0]
+            return
+        # plusieurs resultats. Trier par point le plus petit possible:
+        found.sort()
+        self.result = found[0]
+        return
+
+    def fill_plateau(self, check=False):
+        """ invoquer quand self.result est disponible: compute alors
+            et retourne le plateau avec le carre rempli
+            si check: verifie que la zone ecrite est bien vide
+        """
+        point, taille = self.result
+        ligne_pleine = [self.plein] * taille
+        lilist = [list(line) for line in self.plateau]
+        for y in range(point.y, point.y + taille):
+            if check:
+                assert set(lilist[y][point.x:point.x + taille]) == {'.'}
+            lilist[y][point.x:point.x + taille] = ligne_pleine
+
+        return lilist
+
+    def print_plateau(self, plateau=None):
+        """ print le plateau fourni, ou si none
+        le plateau self.plateau
+
+        plateau = None sert en debug
+        """
+
+        if not plateau:
+            plateau = self.plateau
+        for line in plateau:
+            print(''.join(line))
+
+
+def main():
+    """ pour debug
+    """
+    instance = inFile('example_file')
+    instance.scan_brutal()
+    plateau = instance.fill_plateau()
+    instance.print_plateau(plateau)
+    return instance
---
+++ tests/tf/test_ti.py
@@ -0,0 +1,78 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+""" pytest compliant functional test file
+    fix: logger les résultats et les étapes, sur stderr, pour permettre
+    la surveillance du process, et/ou un rapport de test
+"""
+
+import pytest
+import subprocess as sp
+
+params = [(5, 5, 1), (11, 11, 1), (8, 4, 2), (113, 83, 5)]
+
+
+@pytest.fixture(scope="module", params=params)
+def map(request):
+    """ generate a map file based on params
+    """
+
+    command = ['./scripts/map_gen.py'] + [str(x) for x in request.param]
+    process = sp.Popen(command, stdout=sp.PIPE)
+    return process.stdout.read().decode()
+
+
+def map1(params):
+    """ almost same as map, with a single set of params.
+    some factoring needed here, no time to think it out.
+    """
+
+    command = ['./scripts/map_gen.py'] + [str(x) for x in params]
+    process = sp.Popen(command, stdout=sp.PIPE)
+    return process.stdout.read().decode()
+
+
+def check_result(result):
+    """ tester que result comporte bien un carre
+
+    tester que le carre repond bien au probleme
+    est un peu hors de portee de l'exercice
+    """
+    lines = result.split()
+    xlines = [l for l in lines if 'x' in l]
+    size = len(xlines)
+    chunk = 'x' * size
+    toomuch = 'x' * (size + 1)
+    for xline in xlines:
+        assert chunk in xline
+        assert toomuch not in xline
+
+
+class Test_Nominal:
+    def test_one(self, map):
+        # write the map file:
+        with open('current_test_map_file', 'w') as cmf:
+            cmf.write(map)
+        command = ['./scripts/find_square', 'current_test_map_file']
+        process = sp.Popen(command, stdout=sp.PIPE)
+        result = process.stdout.read().decode()
+        assert result
+        with open('current_test_result', 'w') as f:
+            f.write(result)
+        check_result(result)
+
+    def test_many(self):
+        # write the map files:
+        fnames = []
+        for index, param in enumerate(params):
+            fname = f'current_test_map_file{index}'
+            fnames.append(fname)
+            map = map1(param)
+            with open(fname, 'w') as cmf:
+                cmf.write(map)
+        command = ['./scripts/find_square'] + fnames
+        process = sp.Popen(command, stdout=sp.PIPE)
+        result = process.stdout.read().decode()
+        assert result
+        with open('many_test_result', 'w') as f:
+            f.write(result)
